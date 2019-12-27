8dc2d9e28fad79510adcac84e693e9e79a77cacc
golan1202 <golan1202@gmail.com>
2019-12-27 20:32:17 +0200

    changing runner class to include all cases

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -6,10 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"classpath:FeatureFiles/Orders.feature"},
+@CucumberOptions(features={"classpath:FeatureFiles/Login.feature",
+		"classpath:FeatureFiles/Orders.feature",
+		"classpath:FeatureFiles/Register.feature",
+		"classpath:FeatureFiles/Search.feature"},
 glue={"classpath:com.tutorialsninja.automation.stepdef"},
-plugin={"html:target/cucumber_html_report"},
-tags= {"@Orders","@One"}
+plugin={"html:target/cucumber_html_report"}
+
 
 )
 public class Runner {
